9fe0cd1c5b27ab18f6f65fe914573c67b7587303
mikeiasgabriel <contato.mikeiasgabriel@gmail.com>
2020-03-25 17:35:40 -0300

    Dev frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,9 @@ const cors = require('cors');
 const app = express();
 
 //Avisa para o express que estamos utilizando o formato json
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+
 
 app.listen(3333);  

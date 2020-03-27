1d57dcc09ffc67a126d767ab26a48d84d97dfc51
mikeiasgabriel <contato.mikeiasgabriel@gmail.com>
2020-03-27 13:22:31 -0300

    Update validation and test with Jest

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -6,16 +7,40 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 //Rotas para ongs
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}),OngController.create);
 routes.get('/ongs', OngController.index);
 
 //Rotas para incidents
+/*Validar o envio de novos incidents com authorization e verificação
+ * do objeto
+ */
 routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  }),
+}),IncidentController.index);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  }),
+}),IncidentController.delete);
 
 //Rota para profile
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}),ProfileController.index);
 
 //Rota para login
 routes.post('/session', SessionController.create);
